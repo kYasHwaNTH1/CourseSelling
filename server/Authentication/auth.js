5d431db0c9b45f33007cbca6056468f0d35a6066
kYasHwaNTH1 <kesagoniyashwanth@gmail.com>
2024-09-24 20:17:24 +0530

    all bug free code write now

--- server/Authentication/auth.js
+++ server/Authentication/auth.js
@@ -1,11 +1,11 @@
 const Secured='TIllybohlthepubli342q@@'
-
+const jwt=require('jsonwebtoken')
 const auth=function auth(req,res,next){
     const token=req.headers.token;
     if(!token){
         return res.json({message:"Invalid credentials"})
     }
-    const decoded=jwt.verify({token},Secured)
+    const decoded=jwt.verify(token,Secured)
      if(decoded){
         req.decode=decoded.id;
      }
